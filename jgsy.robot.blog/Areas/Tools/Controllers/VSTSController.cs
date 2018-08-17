7f86ac2b57255b513055d2a4b5505be75cc77214
小萝卜儿头 <18810114757@163.com>
2018-08-17 15:00:17 +0800

    GitHub和VSTS 开源代码双向同步--v0.0.1

---
+++ jgsy.robot.blog/Areas/Tools/Controllers/VSTSController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace jgsy.robot.blog.Areas.Tools.Controllers
+{
+    [Area("Tools")]
+    public class VSTSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- jgsy.robot.blog/Startup.cs
+++ jgsy.robot.blog/Startup.cs
@@ -48,13 +48,19 @@ namespace jgsy.robot.blog
 
             app.UseStaticFiles();
             app.UseCookiePolicy();
-
+            
             app.UseMvc(routes =>
             {
+                routes.MapRoute(
+                name: "Tools",
+                template: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+              );
+
                 routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+          
         }
     }
 }
